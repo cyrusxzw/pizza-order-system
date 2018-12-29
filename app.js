b5611f14ba4cb47654668b06968ec7490748cd5b
Zengwei Xu <xuzengwei06@gmail.com>
2018-12-29 19:55:36 +1100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var port = process.env.PORT || 8000;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -39,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+server.listen(port, function() {
+    console.log("App is running on port " + port);
+});
 module.exports = app;
